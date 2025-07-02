3e5dbbea06a325572da784a565fea9fa83d92d43
Milan Vlaski <milan.vlaski321@gmail.com>
2025-07-02 17:11:03 +0200

    feat: draw some software system boxes

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const softwareSystemBtn = document.getElementById("placeSoftwareSystem")
+const canvas = document.getElementById("canvas");
 
 softwareSystemBtn.addEventListener("click", function () {
     const softwareSystem = {
@@ -19,7 +20,36 @@ softwareSystemBtn.addEventListener("click", function () {
     this.dispatchEvent(placingEvent);
 });
 
+let canvasState = { name: "DEFAULT"}
+
 document.addEventListener("placingEvent", (e) => {
-    console.log("Selected system:", e.detail.object.name);
-    console.log("Description:", e.detail.object.description);
+    const object = e.detail.object
+    console.log("Selected system:", object.name);
+    
+    canvasState = {
+        name: "PLACING",
+        object: object,
+        click: function(e, canvas) {
+            const objectDiv = document.createElement('div')
+            objectDiv.style.position = "absolute"
+            objectDiv.style.width = 100 + 'px'
+            objectDiv.style.height = 100 + 'px'
+            objectDiv.style.left = e.clientX + 'px'
+            objectDiv.style.top = e.clientY + 'px'
+            objectDiv.style.backgroundColor = "red"
+
+            const nameDiv = document.createElement('div')
+            nameDiv.textContent = object.name
+
+            objectDiv.appendChild(nameDiv)
+
+            canvas.appendChild(objectDiv);
+
+            canvasState = {name: "DEFAULT"}
+        },
+    }
 });
+
+canvas.addEventListener("click", (e) => {
+    canvasState.click(e, canvas)
+})
