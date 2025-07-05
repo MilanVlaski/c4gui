338177298dd91423aa952bbf0c0b18a13589d82a
Milan Vlaski <milan.vlaski321@gmail.com>
2025-07-05 11:08:03 +0200

    feat: clean up javascript, and extract html creation function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let canvasState = { name: 'DEFAULT' }
+const DEFAULT_STATE = {name: 'DEFAULT', click: function () {} };
+let canvasState = DEFAULT_STATE
 const canvas = document.getElementById('canvas');
 const softwareSystemBtn = document.getElementById('placeSoftwareSystem')
 
@@ -15,24 +16,28 @@ softwareSystemBtn.addEventListener('click', function () {
         name: 'PLACING',
         object: softwareSystem,
         click: function (e, canvas) {
-            const objectDiv = document.createElement('div')
-            objectDiv.style.position = 'absolute'
-            objectDiv.style.left = e.clientX + 'px'
-            objectDiv.style.top = e.clientY + 'px'
-            objectDiv.classList.add('softwareSystem')
-
-            const nameDiv = document.createElement('div')
-            nameDiv.textContent = softwareSystem.name
-            nameDiv.contentEditable = true
-
-            objectDiv.appendChild(nameDiv)
-
+            const objectDiv = softwareSystemHtml(e, softwareSystem);
             canvas.appendChild(objectDiv);
-            canvasState = { name: 'DEFAULT' }
+            canvasState = DEFAULT_STATE
         },
     }
 });
 
+function softwareSystemHtml(coords, softwareSystem) {
+    let div = document.createElement('div')
+    div.style.position = 'absolute'
+    div.style.left = coords.clientX + 'px'
+    div.style.top = coords.clientY + 'px'
+    div.classList.add('softwareSystem')
+
+    const nameDiv = document.createElement('div')
+    nameDiv.textContent = softwareSystem.name
+    nameDiv.contentEditable = true
+
+    div.appendChild(nameDiv)
+
+    return div
+}
 
 canvas.addEventListener('click', (e) => {
     canvasState.click(e, canvas)
