dffc561cda00ba8ba2a589b07536acf42678abfe
Milan Vlaski <milan.vlaski321@gmail.com>
2025-07-05 10:52:25 +0200

    fix: remove unnecessary code

--- script.js
+++ script.js
@@ -1,52 +1,38 @@
-let canvasState = { name: "DEFAULT"}
-const canvas = document.getElementById("canvas");
-const softwareSystemBtn = document.getElementById("placeSoftwareSystem")
+let canvasState = { name: 'DEFAULT' }
+const canvas = document.getElementById('canvas');
+const softwareSystemBtn = document.getElementById('placeSoftwareSystem')
 
-softwareSystemBtn.addEventListener("click", function () {
+softwareSystemBtn.addEventListener('click', function () {
+
     const softwareSystem = {
-        name: "Software System",
-        description: "",
+        name: 'Software System',
+        description: '',
     };
 
     console.log(`Placing ${softwareSystem.name}`);
 
-    const placingEvent = new CustomEvent("placingEvent", {
-        detail: {
-            element: this,
-            object: softwareSystem,
-        },
-        bubbles: true
-    });
-
-    this.dispatchEvent(placingEvent);
-});
-
-document.addEventListener("placingEvent", (e) => {
-    const object = e.detail.object
-    console.log("Selected object:", object.name);
-    
     canvasState = {
-        name: "PLACING",
-        object: object,
-        click: function(e, canvas) {
+        name: 'PLACING',
+        object: softwareSystem,
+        click: function (e, canvas) {
             const objectDiv = document.createElement('div')
-            objectDiv.style.position = "absolute"
+            objectDiv.style.position = 'absolute'
             objectDiv.style.left = e.clientX + 'px'
             objectDiv.style.top = e.clientY + 'px'
             objectDiv.classList.add('softwareSystem')
 
             const nameDiv = document.createElement('div')
-            nameDiv.textContent = object.name
+            nameDiv.textContent = softwareSystem.name
 
             objectDiv.appendChild(nameDiv)
 
             canvas.appendChild(objectDiv);
-
-            canvasState = {name: "DEFAULT"}
+            canvasState = { name: 'DEFAULT' }
         },
     }
 });
 
-canvas.addEventListener("click", (e) => {
+
+canvas.addEventListener('click', (e) => {
     canvasState.click(e, canvas)
 })
